20e89ef06a9b119c1389966fbd447c27cf34d1e8
michaell2002 <e0900731@u.nus.edu>
2023-03-03 10:13:46 +0800

    mainly boilerplate code for guest and table

---
+++ IS3106WeddingPlanner-ejb/src/java/entity/Table.java
@@ -0,0 +1,123 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entity;
+
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ *
+ * @author leomi
+ */
+@Entity
+public class Table implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    @Column(nullable = false)
+    private int tableNumber;
+    @Column(nullable = false)
+    private double locationX;
+    @Column(nullable = false)
+    private double locationY;
+    @Column(nullable = false)
+    private double size;
+    @Column(nullable = false)
+    private int capacity;
+    @Column(nullable = false)
+    private int currOccupancy;
+
+
+    public Table() {}
+
+    public int getTableNumber() {
+        return tableNumber;
+    }
+
+    public void setTableNumber(int tableNumber) {
+        this.tableNumber = tableNumber;
+    }
+
+    public double getLocationX() {
+        return locationX;
+    }
+
+    public void setLocationX(double locationX) {
+        this.locationX = locationX;
+    }
+
+    public double getLocationY() {
+        return locationY;
+    }
+
+    public void setLocationY(double locationY) {
+        this.locationY = locationY;
+    }
+
+    public double getSize() {
+        return size;
+    }
+
+    public void setSize(double size) {
+        this.size = size;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
+
+    public int getCurrOccupancy() {
+        return currOccupancy;
+    }
+
+    public void setCurrOccupancy(int currOccupancy) {
+        this.currOccupancy = currOccupancy;
+    }
+    
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Table)) {
+            return false;
+        }
+        Table other = (Table) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "entity.Table[ id=" + id + " ]";
+    }
+    
+}
--- IS3106WeddingPlanner-ejb/src/java/entity/WeddingTask.java
+++ IS3106WeddingPlanner-ejb/src/java/entity/WeddingTask.java
@@ -43,6 +43,7 @@ public class WeddingTask implements Serializable {
     @OneToMany(orphanRemoval = true, cascade = { CascadeType.REMOVE, CascadeType.PERSIST }, mappedBy = "parentTask")
     private List<WeddingTask> subtasks;
 
+    public WeddingTask() {}
     public String getTaskDescription() {
         return taskDescription;
     }
