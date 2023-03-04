efd09d1c0f3dbba630bf9eb929effa47d1c5409e
JoelTanCH <joeltanconghan@gmail.com>
2023-03-04 16:47:39 +0800

    Added Entites: Vendor, Request, and Transaction. Added Category Enum class

---
+++ IS3106WeddingPlanner-ejb/src/java/entity/Transaction.java
@@ -0,0 +1,130 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entity;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.Date;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+/**
+ *
+ * @author joelt
+ */
+@Entity
+public class Transaction implements Serializable {
+
+    private static long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long transactionId;
+    @Column(nullable = false)
+    private BigDecimal totalPrice; 
+    @Column(nullable = false)
+    private boolean isPaid = false; 
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date transactionTime;
+    
+    @OneToOne
+    private Request request; 
+
+    public Long getTransactionId() {
+        return transactionId;
+    }
+
+    public void setTransactionId(Long transactionId) {
+        this.transactionId = transactionId;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (getTransactionId() != null ? getTransactionId().hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the transactionId fields are not set
+        if (!(object instanceof Transaction)) {
+            return false;
+        }
+        Transaction other = (Transaction) object;
+        if ((this.getTransactionId() == null && other.getTransactionId() != null) || (this.getTransactionId() != null && !this.transactionId.equals(other.transactionId))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "entity.Transaction[ id=" + getTransactionId() + " ]";
+    }
+
+    /**
+     * @return the serialVersionUID
+     */
+    public static long getSerialVersionUID() {
+        return serialVersionUID;
+    }
+
+    /**
+     * @param aSerialVersionUID the serialVersionUID to set
+     */
+    public static void setSerialVersionUID(long aSerialVersionUID) {
+        serialVersionUID = aSerialVersionUID;
+    }
+
+    /**
+     * @return the totalPrice
+     */
+    public BigDecimal getTotalPrice() {
+        return totalPrice;
+    }
+
+    /**
+     * @param totalPrice the totalPrice to set
+     */
+    public void setTotalPrice(BigDecimal totalPrice) {
+        this.totalPrice = totalPrice;
+    }
+
+    /**
+     * @return the isPaid
+     */
+    public boolean isIsPaid() {
+        return isPaid;
+    }
+
+    /**
+     * @param isPaid the isPaid to set
+     */
+    public void setIsPaid(boolean isPaid) {
+        this.isPaid = isPaid;
+    }
+
+    /**
+     * @return the transactionTime
+     */
+    public Date getTransactionTime() {
+        return transactionTime;
+    }
+
+    /**
+     * @param transactionTime the transactionTime to set
+     */
+    public void setTransactionTime(Date transactionTime) {
+        this.transactionTime = transactionTime;
+    }
+    
+}
---
+++ IS3106WeddingPlanner-ejb/src/java/entity/Vendor.java
@@ -0,0 +1,180 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entity;
+
+import java.io.Serializable;
+import java.util.List;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+/**
+ *
+ * @author joelt
+ */
+@Entity
+public class Vendor implements Serializable {
+
+    private static long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long vendorId;
+    @Column(nullable = false)
+    private String description;
+    @Column(nullable = false)
+    private String banner;
+    @Column(nullable = false)
+    private String websiteUrl;
+    @Column(nullable = false)
+    private String instagramUrl;
+    @Column(nullable = false)
+    private String facebookUrl;
+    @Column(nullable = false)
+    private String whatsappUrl; 
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private CategoryEnum category; 
+    
+    @OneToMany(mappedBy="vendor")
+    private List<Request> requests;
+     
+    
+    public Long getVendorId() {
+        return vendorId;
+    }
+
+    public void setVendorId(Long vendorId) {
+        this.vendorId = vendorId;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (vendorId != null ? vendorId.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the vendorId fields are not set
+        if (!(object instanceof Vendor)) {
+            return false;
+        }
+        Vendor other = (Vendor) object;
+        if ((this.vendorId == null && other.vendorId != null) || (this.vendorId != null && !this.vendorId.equals(other.vendorId))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "entity.Vendor[ id=" + vendorId + " ]";
+    }
+
+    /**
+     * @return the serialVersionUID
+     */
+    public static long getSerialVersionUID() {
+        return serialVersionUID;
+    }
+
+    /**
+     * @param aSerialVersionUID the serialVersionUID to set
+     */
+    public static void setSerialVersionUID(long aSerialVersionUID) {
+        serialVersionUID = aSerialVersionUID;
+    }
+    /**
+     * @return the description
+     */
+    public String getDescription() {
+        return description;
+    }
+
+    /**
+     * @param description the description to set
+     */
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    /**
+     * @return the banner
+     */
+    public String getBanner() {
+        return banner;
+    }
+
+    /**
+     * @param banner the banner to set
+     */
+    public void setBanner(String banner) {
+        this.banner = banner;
+    }
+
+    /**
+     * @return the websiteUrl
+     */
+    public String getWebsiteUrl() {
+        return websiteUrl;
+    }
+
+    /**
+     * @param websiteUrl the websiteUrl to set
+     */
+    public void setWebsiteUrl(String websiteUrl) {
+        this.websiteUrl = websiteUrl;
+    }
+
+    /**
+     * @return the instagramUrl
+     */
+    public String getInstagramUrl() {
+        return instagramUrl;
+    }
+
+    /**
+     * @param instagramUrl the instagramUrl to set
+     */
+    public void setInstagramUrl(String instagramUrl) {
+        this.instagramUrl = instagramUrl;
+    }
+
+    /**
+     * @return the facebookUrl
+     */
+    public String getFacebookUrl() {
+        return facebookUrl;
+    }
+
+    /**
+     * @param facebookUrl the facebookUrl to set
+     */
+    public void setFacebookUrl(String facebookUrl) {
+        this.facebookUrl = facebookUrl;
+    }
+
+    /**
+     * @return the whatsappUrl
+     */
+    public String getWhatsappUrl() {
+        return whatsappUrl;
+    }
+
+    /**
+     * @param whatsappUrl the whatsappUrl to set
+     */
+    public void setWhatsappUrl(String whatsappUrl) {
+        this.whatsappUrl = whatsappUrl;
+    }
+    
+}
