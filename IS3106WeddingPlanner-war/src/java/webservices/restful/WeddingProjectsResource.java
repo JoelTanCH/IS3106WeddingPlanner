7d9a73de1bf02118f72f81b30506d020f90e9e8f
ryougi1702 <wangpohshan@gmail.com>
2023-04-04 20:52:53 +0800

    changed WeddingProjectsResource

--- IS3106WeddingPlanner-war/src/java/webservices/restful/WeddingProjectsResource.java
+++ IS3106WeddingPlanner-war/src/java/webservices/restful/WeddingProjectsResource.java
@@ -5,14 +5,24 @@
  */
 package webservices.restful;
 
+import entity.Admin;
+import entity.WeddingProject;
+import error.WeddingProjectNotFoundException;
+import java.util.List;
+import javax.ejb.EJB;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.UriInfo;
 import javax.ws.rs.Produces;
-import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
-import javax.ws.rs.PUT;
 import javax.enterprise.context.RequestScoped;
+import javax.json.Json;
+import javax.json.JsonObject;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.core.GenericEntity;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import session.WeddingProjectSessionBeanLocal;
 
 /**
  * REST Web Service
@@ -26,29 +36,42 @@ public class WeddingProjectsResource {
     @Context
     private UriInfo context;
 
+    @EJB
+    private WeddingProjectSessionBeanLocal weddingProjectSessionBeanLocal;
+
     /**
      * Creates a new instance of WeddingProjectResource
      */
     public WeddingProjectsResource() {
     }
+    
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<WeddingProject> getAllProjects() {
+        List<WeddingProject> weddingProjects = weddingProjectSessionBeanLocal.getAllWeddingProject();
+        
+        for (WeddingProject w : weddingProjects) {
+            
+        }
+    }
 
-//    /**
-//     * Retrieves representation of an instance of webservices.restful.WeddingProjectsResource
-//     * @return an instance of java.lang.String
-//     */
-//    @GET
-//    @Produces(javax.ws.rs.core.MediaType.APPLICATION_JSON)
-//    public String getJson() {
-//        //TODO return proper representation object
-//        throw new UnsupportedOperationException();
-//    }
-//
-//    /**
-//     * PUT method for updating or creating an instance of WeddingProjectsResource
-//     * @param content representation for the resource
-//     */
-//    @PUT
-//    @Consumes(javax.ws.rs.core.MediaType.APPLICATION_JSON)
-//    public void putJson(String content) {
-//    }
+    @GET
+    @Path("/{wProjectId}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getProjectById(@PathParam("wProjectId") Long wProjectId) {
+        try {
+
+            WeddingProject wProject = weddingProjectSessionBeanLocal.getWeddingProject(wProjectId);
+
+            GenericEntity<WeddingProject> entityToReturn = new GenericEntity<WeddingProject>(wProject) {
+            };
+
+            return Response.status(200).entity(entityToReturn).type(MediaType.APPLICATION_JSON).build();
+        } catch (WeddingProjectNotFoundException e) {
+            JsonObject exception = Json.createObjectBuilder().add("error", "Wedding Project with id " + wProjectId + " not found")
+                    .build();
+            return Response.status(404).entity(exception).type(MediaType.APPLICATION_JSON).build();
+        }
+    }
+
 }
